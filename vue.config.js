dc067ca4a007184eef214ce8d972e96131c575fe
Caven Chen <cavencj@gmail.com>
2020-05-14 11:07:14 +0800

    init

--- vue.config.js
+++ vue.config.js
@@ -2,20 +2,21 @@
  * @Author: Caven
  * @Date: 2018-12-15 00:33:19
  * @Last Modified by: Caven
- * @Last Modified time: 2020-05-07 14:48:54
+ * @Last Modified time: 2020-05-14 11:04:08
  */
 'use strict'
 const path = require('path')
+const fs = require('fs-extra')
 
 const CopywebpackPlugin = require('copy-webpack-plugin')
-const dvgisDist = './node_modules/@dvgis/dc-sdk/dist/dc-sdk'
+const dvgis = './node_modules/@dvgis'
 
 let resolve = dir => {
   return path.resolve(__dirname, dir)
 }
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/vue-dc' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
   productionSourceMap: false,
   configureWebpack: {
     module: {
@@ -27,7 +28,7 @@ module.exports = {
   },
   chainWebpack: config => {
     config.resolve.extensions.add('.js').add('.vue')
-    config.resolve.alias.set('dvgis', path.resolve(__dirname, dvgisDist))
+    config.resolve.alias.set('dvgis', path.resolve(__dirname, dvgis))
     config.module
       .rule('images')
       .test(/\.(png|jpe?g|gif)(\?.*)?$/)
@@ -70,10 +71,29 @@ module.exports = {
     config.plugin('copy').use(CopywebpackPlugin, [
       [
         {
-          from: path.join(dvgisDist, 'resources'),
-          to: 'libs/dc-sdk/resources'
+          from: path.join(dvgis, 'dc-sdk/dist/resources'),
+          to: path.join(__dirname, 'dist', 'libs/dc-sdk/resources')
         }
       ]
     ])
+  },
+  pluginOptions: {
+    electronBuilder: {
+      chainWebpackMainProcess: config => {
+        let outputDir = 'dist_electron/bundled'
+        fs.removeSync(path.join(__dirname, outputDir, 'Assets'))
+        fs.removeSync(path.join(__dirname, outputDir, 'Widgets'))
+        fs.removeSync(path.join(__dirname, outputDir, 'Workers'))
+        fs.removeSync(path.join(__dirname, outputDir, 'ThirdParty'))
+        config.plugin('copy').use(CopywebpackPlugin, [
+          [
+            {
+              from: path.join(dvgis, 'dc-sdk/dist/resources'),
+              to: path.join(__dirname, outputDir, 'libs/dc-sdk/resources')
+            }
+          ]
+        ])
+      }
+    }
   }
 }
